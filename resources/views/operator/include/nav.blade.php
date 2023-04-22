9c3c65d4e24a3d275a14a73e6fe86503cf58314e
shivam <shivammawai80@gmail.com>
2023-04-23 00:47:14 +0530

    chages

--- resources/views/operator/include/nav.blade.php
+++ resources/views/operator/include/nav.blade.php
@@ -11,35 +11,39 @@
 				<div class="collapsible-body">
 					<ul class="collapsible" data-collapsible="accordion">
 						@foreach($access_menu as $menu)
-						<li class="bold @if(in_array(Request::segment(1).Request::segment(2).Request::segment(3).Request::segment(4) , getMenuUrl($menu->id), true)) {{ 'active' }} @endif">
-							<a class="collapsible-header waves-effect waves-cyan " href="{{URL($menu->path)}}"><i class="material-icons">{{$menu->icon}}</i><span class="menu-title" data-i18n="">{{$menu->name}}</span>
-								@if(@$menu->childs->count() > 0) <span class="fa arrow menu_hide"></span>@endif
-							</a>
-							@if($menu->childs->count() > 0)
-									<div class="collapsible-body">
-										<ul class="collapsible" data-collapsible="accordion">
-			            					@foreach($menu->childs as $child)
-			            						@if($child->path == '#')
-												<li class="bold @if(Request::segment(1).Request::segment(2).Request::segment(3).Request::segment(4) == str_replace('/','',$child->path)) {{ 'active' }} @endif"><a class="collapsible-body" href="{{URL::to($child->path)}}" data-i18n=""><i class="material-icons">radio_button_unchecked</i><span>{{$child->name}}</span>
-			            								@if(@$child->sub_childs->count() > 0) <span class="fa arrow menu_hide"></span>@endif</a>
-			            								@if($child->sub_childs->count() > 0)
-			            									<div class="collapsible-body">
-																<ul class="collapsible" data-collapsible="accordion">
-					            									@foreach($child->sub_childs as $child)
-					            										<li class="bold @if(Request::segment(1).Request::segment(2).Request::segment(3).Request::segment(4) == str_replace('/','',$child->path)) {{ 'activemenu' }} @endif"><a class="collapsible-body" href="{{URL::to($child->path)}}" data-i18n=""><i class="material-icons">radio_button_unchecked</i><span>{{$child->name}}</span></a></li>
-					            									@endforeach
-					            								</ul>
-					            							</div>
-			            								@endif
-			            							</li>
-			            						@else
-			            							<li class="bold @if(Request::segment(1).Request::segment(2).Request::segment(3).Request::segment(4) == str_replace('/','',$child->path)) {{ 'activemenu' }} @endif"><a class="collapsible-body" href="{{URL::to($child->path)}}" data-i18n=""><i class="material-icons">radio_button_unchecked</i><span>{{$child->name}}</span></a></li>
-			            						@endif
-			            					@endforeach
-			          					</ul>
-			        				</div>
-							@endif
-			        		</li>
+							@if($menu->path == '#')
+								<li class="bold @if(in_array(Request::segment(1).Request::segment(2).Request::segment(3).Request::segment(4) , getMenuUrl($menu->id), true)) {{ 'active' }} @endif">
+									<a class="collapsible-header waves-effect waves-cyan " href="{{URL($menu->path)}}"><i class="material-icons">{{$menu->icon}}</i><span class="menu-title" data-i18n="">{{$menu->name}}</span>
+										@if(@$menu->childs->count() > 0) <span class="fa arrow menu_hide"></span>@endif
+									</a>
+									@if($menu->childs->count() > 0)
+											<div class="collapsible-body">
+												<ul class="collapsible" data-collapsible="accordion">
+					            					@foreach($menu->childs as $child)
+					            						@if($child->path == '#')
+														<li class="bold @if(Request::segment(1).Request::segment(2).Request::segment(3).Request::segment(4) == str_replace('/','',$child->path)) {{ 'active' }} @endif"><a class="collapsible-body" href="{{URL::to($child->path)}}" data-i18n=""><i class="material-icons">radio_button_unchecked</i><span>{{$child->name}}</span>
+					            								@if(@$child->sub_childs->count() > 0) <span class="fa arrow menu_hide"></span>@endif</a>
+					            								@if($child->sub_childs->count() > 0)
+					            									<div class="collapsible-body">
+																		<ul class="collapsible" data-collapsible="accordion">
+							            									@foreach($child->sub_childs as $child)
+							            										<li class="bold @if(Request::segment(1).Request::segment(2).Request::segment(3).Request::segment(4) == str_replace('/','',$child->path)) {{ 'activemenu' }} @endif"><a class="collapsible-body" href="{{URL::to($child->path)}}" data-i18n=""><i class="material-icons">radio_button_unchecked</i><span>{{$child->name}}</span></a></li>
+							            									@endforeach
+							            								</ul>
+							            							</div>
+					            								@endif
+					            							</li>
+					            						@else
+					            							<li class="bold @if(Request::segment(1).Request::segment(2).Request::segment(3).Request::segment(4) == str_replace('/','',$child->path)) {{ 'activemenu' }} @endif"><a class="collapsible-body" href="{{URL::to($child->path)}}" data-i18n=""><i class="material-icons">radio_button_unchecked</i><span>{{$child->name}}</span></a></li>
+					            						@endif
+					            					@endforeach
+					          					</ul>
+					        				</div>
+									@endif
+				        		</li>
+				        	@else
+				        		<li class="bold @if(Request::segment(1).Request::segment(2).Request::segment(3).Request::segment(4) == str_replace('/','',$menu->path)) {{ 'activemenu' }} @endif"><a class="collapsible-body" href="{{URL::to($menu->path)}}" data-i18n=""><i class="material-icons">radio_button_unchecked</i><span>{{$menu->name}}</span></a></li>
+				        	@endif
 						@endforeach
 					</ul>
 				</div>
